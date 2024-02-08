5054e0c33ccab73491f9f60b7e887a75f2117fb2
vsm19 <victorsanchezmula@gmail.com>
2024-02-08 11:41:06 +0100

    Initial commit

---
+++ app/src/main/java/com/plcoding/weatherapp/presentation/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.plcoding.weatherapp.presentation.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBlue = Color(0xFF1B3B5A)
+val DeepBlue = Color(0xFF102840)
+val rosaOscuro = Color(0xFFF48FB1)
+val rosaClaro = Color(0xFFF8BBD0)
